d1fb95c3b9314009dcdcc9f9a3005127622eb605
sphantom99 <58749624+sphantom99@users.noreply.github.com>
2022-01-10 18:20:20 +0200

    fixed footer

--- components/footer.js
+++ components/footer.js
@@ -10,7 +10,7 @@ export default function AppFooter() {
         left: 0,
         right: 0,
         bottom: 0,
-        position: 'absolute',
+        position: 'relative',
       }}
     >
       <Box
